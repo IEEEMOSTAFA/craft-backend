691bbf1b65a31268c4bb596c3ff17ee51a6a9288
Md Al Mostafa <skmostafa8888@gmail.com>
2025-05-25 00:16:00 +0600

    correction added

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 require('dotenv').config();
 const app = express()
@@ -35,7 +35,8 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    const userCollection = client.db('craftStore').collection('products');
+    const productCollection = client.db('craftStore').collection('products');
+    const registerCollection = client.db('register').collection('visitors');
 
 
 
@@ -43,12 +44,81 @@ async function run() {
     // crud operation start::
 
     // Create:
-    app.post('addCraft', async(req,res) =>{
+    app.post('/addCraft', async(req,res) =>{
      const user = req.body;
      console.log('New User',user);
 
-     const result = userCollection1.insertOne(user)
+     const result = await productCollection.insertOne(user)
+     console.log(result);
      res.send(result);
+    })
+    // create for register:
+    app.post('/addRegister', async(req,res) =>{
+     const user = req.body;
+     console.log('New User',user);
+
+     const result = await registerCollection.insertOne(user)
+     console.log(result);
+     res.send(result);
+    })
+
+    // Read:
+    
+    app.post('/addCraft', async(req,res) =>{
+     const user = req.body;
+     console.log('New User',user);
+
+     const result = await productCollection.insertOne(user)
+     console.log(result);
+     res.send(result);
+    })
+
+    // Read:
+    app.get("/myCraft/:email", async (req, res) =>{
+      const email = req.params.email;
+      console.log('getting user email',email);
+
+      const result = await productCollection.find({email:req.params.email}).toArray()
+      res.send(result);
+
+     
+
+    })
+    // Read:
+    app.get("/singleproducts/:id", async (req, res) =>{
+      const result = await productCollection.findOne({_id: new ObjectId(req.params.id)});
+      res.send(result);
+
+    })
+
+    //Update
+    app.put("/updateProduct/:id",async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      console.log('updating user',id);
+      const data = {
+        $set: {
+          name: req.body.name,
+          price: req.body.price,
+          brandName: req.body.brandName,
+          imgUrl: req.body.imgUrl,
+          type: req.body.type,
+        }
+      }
+      const result = await productCollection.updateOne(query,data);
+      console.log(result);
+      res.send(result);
+    })
+
+    // Delete:
+    app.delete("/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const result = await productCollection.deleteOne(filter);
+      console.log(result);
+      res.send(result);
+
+
     })
 
 
